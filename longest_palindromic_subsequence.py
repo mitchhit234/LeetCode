14cb3e6af5811307f5f8c43955cf295ae6c154db
Mitchell Meier <mitchhit@gmail.com>
2022-04-12 11:53:55 -0500

    lazy git commit

---
+++ longest_palindromic_subsequence.py
@@ -0,0 +1,20 @@
+class Solution:
+
+  def longestPalindromeSubseq(self, s):
+    
+    dp = [[0 for _ in range(len(s))] for _ in range(len(s))]
+
+    for i in range(len(s)-1,-1,-1):
+      dp[i][i] = 1
+      for j in range(i+1,len(s)):
+        if s[i] == s[j]:
+          dp[i][j] = dp[i+1][j-1] + 2
+        else:
+          dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+
+    return dp[0][-1]
+
+
+obj = Solution()
+s = "bbbab"
+print(obj.longestPalindromeSubseq(s))
